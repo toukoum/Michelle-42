0bb0472eceb609ce1f7b15b66ed2b0eea8640a0a
Kevin Trevis <ketrevis@student.42.fr>
2024-02-21 16:40:43 +0100

    shlvl incrementation

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,10 @@
 /*   By: ketrevis <ketrevis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 21:50:45 by ketrevis          #+#    #+#             */
-/*   Updated: 2024/02/15 15:35:01 by ketrevis         ###   ########.fr       */
+/*   Updated: 2024/02/21 16:10:23 by ketrevis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "minishell.h"
 
 int	main(int ac, char **av, char **env)
