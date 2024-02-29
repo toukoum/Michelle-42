f5e54de46a63c0d91357857f836e1217f4b14350
Kevin Trevis <ketrevis@bess-f4r2s12.clusters.42paris.fr>
2024-02-29 13:48:54 +0100

    leak fix

--- srcs/parse_input.c
+++ srcs/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: ketrevis <ketrevis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 12:09:43 by ketrevis          #+#    #+#             */
-/*   Updated: 2024/02/29 12:14:01 by ketrevis         ###   ########.fr       */
+/*   Updated: 2024/02/29 13:48:50 by ketrevis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ int	parse_input(char *input, t_env **env, int *res)
 	char	***split_arr;
 
 	if (!pipe_valid(input))
-		return (1);
+		return (free(input), 1);
 	if (!quote_closed(input))
 		return (free(input), SYNTAX_ERROR);
 	input = replace_var_names(input, *env, *res);
