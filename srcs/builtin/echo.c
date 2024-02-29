0746c13cfc6406e9c8ce06d859a4378193f6e207
Kevin Trevis <ketrevis@bess-f4r2s12.clusters.42paris.fr>
2024-02-29 13:47:29 +0100

    echo fix

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgiraud <rgiraud@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ketrevis <ketrevis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 17:25:59 by ketrevis          #+#    #+#             */
-/*   Updated: 2024/02/27 19:54:58 by rgiraud          ###   ########.fr       */
+/*   Updated: 2024/02/29 13:47:02 by ketrevis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	ft_echo(char **cmd)
 
 	i = 1;
 	flag = false;
-	while (cmd[i] && cmd[i][0] == '-' && only_n(cmd[i + 1]))
+	while (cmd[i] && cmd[i][0] == '-' && only_n(cmd[i] + 1))
 	{
 		flag = true;
 		i++;
